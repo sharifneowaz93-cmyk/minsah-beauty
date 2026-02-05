9f92a71a30b2670787046a4a732963340379b558
sharifneowaz93-cmyk <sharifneowaz93@gmail.com>
2026-02-05 14:34:05 -0800

    Refactor prisma.config.ts to use process.env

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,16 +1,30 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: "npx tsx prisma/seed.ts",
   },
   datasource: {
-    url: env("DATABASE_URL"),
-    shadowDatabaseUrl: env("SHADOW_DATABASE_URL"),
+    url: process.env.DATABASE_URL!,
   },
 });
+
+// import "dotenv/config";
+// import { defineConfig, env } from "prisma/config";
+
+// export default defineConfig({
+//   schema: "prisma/schema.prisma",
+//   migrations: {
+//     path: "prisma/migrations",
+//   },
+//   datasource: {
+//     url: env("DATABASE_URL"),
+//     shadowDatabaseUrl: env("SHADOW_DATABASE_URL"),
+//   },
+// });
 
 // import 'dotenv/config'
 // import { defineConfig, env } from "prisma/config";
